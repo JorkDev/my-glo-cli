9d62a2725f062201d7a3e1a39c7ef4bac369ad56
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-10-02 23:21:48 -0500

    Add error handling to echo command

--- cmd/echo.go
+++ cmd/echo.go
@@ -1,16 +1,25 @@
 package cmd
 
 import (
+    "errors"
     "fmt"
     "github.com/spf13/cobra"
+    "go.uber.org/zap"
+    "github.com/JorkDev/my-go-cli/internal"
 )
 
 var echoCmd = &cobra.Command{
     Use:   "echo [text]",
     Short: "Echo the input text",
     Args:  cobra.MinimumNArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
+    RunE: func(cmd *cobra.Command, args []string) error {
+        if len(args[0]) > 100 {
+            internal.Logger.Error("Text length exceeds 100 characters", zap.String("input", args[0]))
+            return errors.New("input text is too long, must be under 100 characters")
+        }
+
         fmt.Println(args[0])
+        return nil
     },
 }
 
