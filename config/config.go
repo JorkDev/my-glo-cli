968b96165b0907cb96a515d1184392985e72272f
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-10-02 23:09:07 -0500

    Add configuration loading with Viper

--- config/config.go
+++ config/config.go
@@ -1,5 +1,21 @@
 package config
 
+import (
+    "fmt"
+    "github.com/spf13/viper"
+    "log"
+)
+
 func InitConfig() {
-    // Placeholder for future configuration logic
+    viper.SetConfigName("config")
+    viper.SetConfigType("yaml")
+    viper.AddConfigPath(".")
+
+    viper.AutomaticEnv()
+
+    if err := viper.ReadInConfig(); err != nil {
+        log.Fatalf("Error reading config file: %s", err)
+    }
+
+    fmt.Printf("App Name: %s\n", viper.GetString("app_name"))
 }
